ee734eb4c9bf2d2beca5e88db0e1296f41f2c1b0
Dhaval saxena <dhaval.saxena@minddeft.net>
2023-05-22 12:28:39 +0530

    final

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+        '2.1xl': ['1.8rem', { lineHeight: '2rem' }],
+
+      }
+    },
+  },
+  plugins: [],
+}
+
